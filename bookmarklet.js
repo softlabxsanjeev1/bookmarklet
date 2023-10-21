ec70fe265f7a1dcb703e83d51d85f2be71a5dbf2
softlabxsanjeev1 <sanjeevyaadav28520@gmail.com>
2023-10-21 10:26:00 +0530

    first

---
+++ bookmarklet.js
@@ -0,0 +1,9 @@
+//  anonymous function
+(function() {
+    console.log("bookmarklet starting");
+    let paragraphs = document.getElementsByTagName('p')
+    for(let i = 0; i< paragraphs.length; i++){
+        paragraphs[i].innerHTML = 'kitten';
+    }
+})();
+
